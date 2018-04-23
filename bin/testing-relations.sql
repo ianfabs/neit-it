4c2c79a6e22d522698e1fcae3bdcad9597884422
Fabricatore <itfabricatore@labs.neit.edu>
2018-04-23 18:52:59 -0400

    courses work in progress

--- bin/testing-relations.sql
+++ bin/testing-relations.sql
@@ -1,13 +1,15 @@
-SELECT DISTINCT
+SELECT
 Curriculum.Cohorts.CohortId as [Cohort ID],
 Curriculum.Cohorts.SectionNumber as [Section],
 CohortStartDate as [Start Date],
 dbo.getQuarter(CohortStartDate, '201810', Curriculum.Curriculum.CurriculumLevel) AS [Current Quarter],
 CohortStudentCount as [# Students],
 Curriculum.Curriculum.CurriculumLevel,
-Curriculum.CourseSections.CourseDept
+Curriculum.CourseSections.CourseDept,
+Curriculum.CourseSections.CohortID AS [Course Section Cohort]
 FROM Curriculum.Cohorts JOIN Curriculum.Curriculum
 	ON Curriculum.Cohorts.CurriculumId = Curriculum.Curriculum.CurriculumId
 	JOIN Curriculum.CourseSections 
 	ON (Curriculum.CourseSections.SectionNumber = Curriculum.Cohorts.SectionNumber)
 	AND (Curriculum.CourseSections.CohortID = Curriculum.Cohorts.CohortId)
+ORDER BY Curriculum.CourseSections.CohortID ASC
--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,7 @@ var knex = require('knex')({
   }
 });
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Schedule'});
@@ -26,8 +27,6 @@ router.get('/cohorts', function(req, res, next) {
   .from("Curriculum.Cohorts").join("Curriculum.Curriculum", "Curriculum.Curriculum.CurriculumID", "=", "Curriculum.Cohorts.CurriculumId").then( (row) => {
     res.json(row);
   } );
-
-  
 });
 
 router.get('/cohorts/:id', function(req, res, next) {
@@ -36,9 +35,27 @@ router.get('/cohorts/:id', function(req, res, next) {
   } );
 });
 
+router.post('/cohorts/:id', function(req, res, next) {
+  knex.select(knex.raw("CohortId AS [id], CohortStartDate, CohortStudentCount, dbo.getQuarter(Curriculum.Cohorts.CohortStartDate, "+current()+", Curriculum.Curriculum.CurriculumLevel) AS [q], Curriculum.Curriculum.CurriculumID AS [CurriculumID]"))
+  .from("Curriculum.Cohorts").join("Curriculum.Curriculum", "Curriculum.Curriculum.CurriculumID", "=", "Curriculum.Cohorts.CurriculumId").where("CohortId", "=", req.params.id).then( (row) => {
+    res.send(row[0]);
+  } );
+});
+
+router.put('/cohorts/:id/courses', function(req, res, next){
+  //Get Next Quarter Classes
+  console.log(req.body);
+
+  knex.select('CurriculumId','CourseDept', 'CourseId', 'CurriculumQuarter').from('Curriculum.CurriculumCourses').where("CurriculumId", "=", req.body.CurriculumID).andWhere("CurriculumQuarter", "=", req.body.q).then(
+    (row) => {
+      res.send( row );
+    }
+  );
+});
+
 router.get('/instructors', function(req, res, next) {
   knex.select('instructorDisplayName').from('Curriculum.Instructor').where("instructorDisplayName", '<>', 'NULL').orderBy('instructorDisplayName').then( (row)=>{
-    console.log(row);
+    
     res.json(row);
   } );
 });
@@ -51,6 +68,19 @@ router.get('/courses', function(req, res, next) {
 
 
   //res.render('index', { title: 'Schedule', cohorts: row });
+});
+
+router.get('/test', function(req, res, next){
+  //testing knex queries
+  knex.select(
+    knex.raw("CohortId AS [id], CohortStartDate, CohortStudentCount, dbo.getQuarter(Curriculum.Cohorts.CohortStartDate, "+current()+", Curriculum.Curriculum.CurriculumLevel) AS [q], Curriculum.Cohorts.SectionNumber as [section], CohortStudentCount as [students], Curriculum.Curriculum.CurriculumLevel, Curriculum.CourseSections.CourseDept")
+  ).from("Curriculum.Cohorts").join("Curriculum.Curriculum", "Curriculum.Curriculum.CurriculumID", "=", "Curriculum.Cohorts.CurriculumId").join("Curriculum.CourseSections", function(){
+    this.on("Curriculum.CourseSections.SectionNumber", "=", "Curriculum.Cohorts.SectionNumber")
+    .andOn("Curriculum.CourseSections.CohortID", "=", "Curriculum.Cohorts.CohortId")
+  });
+  
+
+
 });
 
 module.exports = router;
